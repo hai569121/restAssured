dce76c23cf34366246e165235e0f6431f550897b
59612 <596121023@qq.com>
2025-09-03 17:00:06 +0800

    方法名修改

---
+++ src/test/java/copyTest.java
@@ -0,0 +1,48 @@
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.*;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@Tag("smoke")
+public class copyTest {
+
+    @Test
+    @Order(1)
+    public void postTest1() {
+        //Map<String, String> headers = new HashMap<>();
+        //headers.put("Content-Type", "application/json;charset=UTF-8");
+        String JsonString = "{\"title\":\"foo\",\"name\":\"张三\"}";
+        given().
+                contentType(ContentType.JSON).
+                body(JsonString).
+                when().
+                post("https://jsonplaceholder.typicode.com/posts").
+                then().
+                log().
+                all().
+                assertThat().body("title",equalTo("foo"));
+
+    }
+
+    @Test
+    @Order(2)
+    public void postTest2() {
+        given().
+                multiPart(new File("C:\\Users\\59612\\Desktop\\1.png")).
+                when().
+                post("http://www.httpbin.org/post").
+                then().
+                log().
+                all();
+
+
+
+    }
+
+}
