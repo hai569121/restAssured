3b432cb7ec39736e481a8bf848de283b1e01b76e
59612 <596121023@qq.com>
2025-09-03 17:23:52 +0800

    方法名修改

--- src/test/java/copyTest.java
+++ src/test/java/copyTest.java
@@ -8,41 +8,21 @@ import java.util.Map;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@Tag("smoke")
+
+
 public class copyTest {
 
     @Test
-    @Order(1)
-    public void postTest1() {
-        //Map<String, String> headers = new HashMap<>();
-        //headers.put("Content-Type", "application/json;charset=UTF-8");
-        String JsonString = "{\"title\":\"foo\",\"name\":\"张三\"}";
+    public void test1() {
         given().
-                contentType(ContentType.JSON).
-                body(JsonString).
-                when().
-                post("https://jsonplaceholder.typicode.com/posts").
-                then().
-                log().
-                all().
-                assertThat().body("title",equalTo("foo"));
 
+                when().
+                get("https://jsonplaceholder.typicode.com/posts/1").
+                then().
+                log().body();
     }
 
-    @Test
-    @Order(2)
-    public void postTest2() {
-        given().
-                multiPart(new File("C:\\Users\\59612\\Desktop\\1.png")).
-                when().
-                post("http://www.httpbin.org/post").
-                then().
-                log().
-                all();
 
-
-
-    }
 
 }
+
